dac861becff7c3e8e1adebe041bdfb01c881a5a7
s2khali <bwschnell52@gmail.com>
2022-12-23 00:20:37 -0600

    Begin day randomizer

--- desktop-image-update.py
+++ desktop-image-update.py
@@ -1,4 +1,6 @@
 import ctypes
+import datetime
+import json
 import os.path
 import threading
 import time
@@ -16,13 +18,15 @@ REDDIT_SECRET = os.getenv("REDDIT_SECRET")
 REDDIT_USERNAME = os.getenv("REDDIT_USERNAME")
 REDDIT_PASSWORD = os.getenv("REDDIT_PASSWORD")
 
-SUBREDDIT = "wallpaper"
+SUBREDDIT = "wallpapers"
 
 USERAGENT = "windows:{}:v1 (by /u/{})".format(REDDIT_ID, REDDIT_USERNAME)
 
 SCREEN_HEIGHT = 1440
 TEMP_LOC = "C:\\Users\\{}\\AppData\\Local\\Temp".format(os.getlogin())
 ALLOWED_TYPES = [".png", ".jpg", ".jpeg"]
+
+TODAY = datetime.date.today()
 
 
 def getimagesize(uri):
@@ -82,16 +86,87 @@ def notify():
     # set here because running this in a thread quits cleaner than this handling it
 
 
+def savedata():
+    exit()
+
+
+def getsaveddata():
+    sampledata = """
+    {
+        "lastupdate": "2022-12-22",
+        "dates": {
+            "2022-12-13": [
+                "https://i.redd.it/6ib1hrf1bu6a1.jpg"
+            ],
+            "2022-12-14": [
+                "https://i.redd.it/6ib1hrf1bu6a1.jpg"
+            ],
+            "2022-12-22": [
+                "https://i.redd.it/6ib1hrf1bu6a1.jpg"
+            ]
+        }
+    }
+    """
+    return json.loads(sampledata)
+
+
+# TODO: Unit test this function
+def clearolddata(data):
+    week_ago = TODAY - datetime.timedelta(days=7)
+
+    for key in list(data["dates"]):
+        converteddate = datetime.datetime.strptime(key, '%Y-%m-%d').date()
+
+        if converteddate < week_ago:
+            del data["dates"][key]
+
+
+def getallimageurls(data):
+    temp = []
+
+    for key in data["dates"].keys():
+        for imageurl in data["dates"][key]:
+            temp.append(imageurl)
+
+    return temp
+
+
+def alreadyrantoday(data):
+    todayfromdata = datetime.datetime.strptime(data["lastupdate"], '%Y-%m-%d').date()
+    if todayfromdata == TODAY:
+        return True
+    return False
+
+
 temp_file_loc = ""
 access_token = getaccesstoken()
+saveddata = getsaveddata()
+if alreadyrantoday(saveddata):
+    print("TODO: Pickle")
+    exit()
+
+clearolddata(saveddata)
+imageurls = getallimageurls(saveddata)
 
 for top_post in getposts(access_token):
-    if top_post['kind'] == "t3" and top_post['data']['url'].find("i.redd.it/") != -1:
-        sizing = getimagesize(top_post['data']['url'])
+    imageurl = top_post['data']['url']
+    if top_post['kind'] == "t3" and imageurl.find("i.redd.it/") != -1:
+        if imageurl in imageurls:
+            continue
+
+        print(imageurl)
+        sizing = getimagesize(imageurl)
         height = sizing[1][1]
 
         if height >= SCREEN_HEIGHT:  # TODO: Make sure resolution matches, not just a minimum height
-            temp_file_loc = downloadphoto(top_post['data']['url'])
+            temp_file_loc = downloadphoto(imageurl)
+
+            todaystring = TODAY.strftime("%Y-%m-%d")
+            if todaystring not in saveddata["dates"].keys():
+                saveddata["dates"][todaystring] = []
+            saveddata["dates"][todaystring].append(imageurl)
+            print(saveddata)
+            exit()
             break
 
 if not temp_file_loc:
